50ce76ddd52026893395508c39e8ee41ebf412ae
alvindioprakosa <alvindioprakosa@gmail.com>
2025-04-03 23:50:24 +0700

    Update Subject Controller

--- app/Http/Controllers/Api/SubjectController.php
+++ app/Http/Controllers/Api/SubjectController.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace App\Http\Controllers\Api;
+
 use App\Http\Controllers\Api\BaseController as BaseController;
 use App\Models\Subject;
 use Illuminate\Http\Request;
+
 class SubjectController extends BaseController
 {
     /**
@@ -13,9 +15,12 @@ class SubjectController extends BaseController
      */
     public function index()
     {
-        $subject = Subject::all();
+        $subjects = Subject::all();
 
-        return $this->sendResponse($subject, 'Success to get data for all subjects');
+        if ($subjects->isEmpty()) {
+            return $this->sendError('No subjects found', [], 404);
+        }
+
+        return $this->sendResponse($subjects, 'Successfully retrieved all subjects');
     }
-
 }
